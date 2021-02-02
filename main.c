9179a30637ddb7d8b51853f2dd245d0ff4b66991
Rance Campbell <rancewcampbell@gmail.com>
2021-02-02 00:47:03 -0700

    program making API calls and getting stock quote data

--- main.c
+++ main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "curl/curl.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
     CURL *curl = curl_easy_init();
+    char URL[256] = "https://finnhub.io/api/v1/quote?symbol=";
+
+    if(argc != 3) {
+       printf("I need a stock symbol and the API key please.\n");
+       return EXIT_FAILURE;
+    }
 
     if (!curl) {
         fprintf(stderr, "init failed\n");
         return EXIT_FAILURE;
     }
-    printf("curl worked\n");
+    if(curl) {
+        CURLcode res;
+        strncat(URL, argv[1], 10);
+        strncat(URL, "&token=", 8);
+        strncat(URL, argv[2], 20);
+        printf("%s\n", URL);
+        curl_easy_setopt(curl, CURLOPT_URL, URL);
+        res = curl_easy_perform(curl);
+    }
     curl_easy_cleanup(curl);
-
+    printf("\n");
     return EXIT_SUCCESS;
 }
