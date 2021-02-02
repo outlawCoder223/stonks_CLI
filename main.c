9cd1535a555a98a84a939ce85972b0a5ecfbaf86
Rance Campbell <rancewcampbell@gmail.com>
2021-02-02 01:05:54 -0700

    add feature to grab API key from env vars

--- main.c
+++ main.c
@@ -6,25 +6,32 @@
 int main(int argc, char *argv[]) {
     CURL *curl = curl_easy_init();
     char URL[256] = "https://finnhub.io/api/v1/quote?symbol=";
+    char *API = getenv("FINNHUB_API_KEY");
+    int API_size = strlen(API);
 
-    if(argc != 3) {
-       printf("I need a stock symbol and the API key please.\n");
-       return EXIT_FAILURE;
+    if(argc < 2) {
+        printf("I need a stock symbol\n");
+        return EXIT_FAILURE;
+    } else if (!API)
+    {
+        printf("Set your FINNHUB_API_KEY env var");
+        return EXIT_FAILURE;
     }
+    
 
     if (!curl) {
         fprintf(stderr, "init failed\n");
         return EXIT_FAILURE;
     }
-    if(curl) {
-        CURLcode res;
-        strncat(URL, argv[1], 10);
-        strncat(URL, "&token=", 8);
-        strncat(URL, argv[2], 20);
-        printf("%s\n", URL);
-        curl_easy_setopt(curl, CURLOPT_URL, URL);
-        res = curl_easy_perform(curl);
-    }
+
+    CURLcode res;
+    strncat(URL, argv[1], 10);
+    strncat(URL, "&token=", 8);
+    strncat(URL, API, API_size);
+    printf("%s\n", URL);
+    curl_easy_setopt(curl, CURLOPT_URL, URL);
+    res = curl_easy_perform(curl);
+    
     curl_easy_cleanup(curl);
     printf("\n");
     return EXIT_SUCCESS;
